b1ebc590a7406a8e6a18d8401f6b9eae55e2cd8a
Kevin de Bree <kpdebree@gmail.com>
2017-10-02 07:26:44 -0700

    Update 2

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+  
+
+//Firebase Info
+  var config = {
+    apiKey: "AIzaSyDx5MQKhrdxkahtQK1Cnd_2w60PEeVSgpk",
+    authDomain: "week-7-homework-454e0.firebaseapp.com",
+    databaseURL: "https://week-7-homework-454e0.firebaseio.com",
+    projectId: "week-7-homework-454e0",
+    storageBucket: "week-7-homework-454e0.appspot.com",
+    messagingSenderId: "437004037928"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
